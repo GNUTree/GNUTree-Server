1dc5b8305fa5c0b0f3a98ccab96e8675b7e01f34
Jang-DongHo <donghoo18@naver.com>
2022-12-13 02:30:06 +0900

    [FIX] secret 파일 삭제

--- src/app/Tree/treeService.js
+++ src/app/Tree/treeService.js
@@ -1,6 +1,5 @@
 const { logger } = require("../../../config/winston");
 const { pool } = require("../../../config/database");
-const secret_config = require("../../../config/secret");
 const treeProvider = require("./treeProvider");
 const treeDao = require("./treeDao");
 const baseResponse = require("../../../config/baseResponseStatus");
