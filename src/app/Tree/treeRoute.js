0dd85b04ef262ce044e129ce7657a3e20a0377bd
Jang-DongHo <donghoo18@naver.com>
2022-12-14 23:11:01 +0900

    [FIX] route 수정

--- src/app/Tree/treeRoute.js
+++ src/app/Tree/treeRoute.js
@@ -4,11 +4,11 @@ module.exports = function (app) {
   const checkUserIdx = require("./treeMiddleware");
 
   // 전체 장식품 조회 API
-  app.get("api/trees/:userIdx", checkUserIdx, tree.getDecorations);
+  app.get("/api/trees/:userIdx", checkUserIdx, tree.getDecorations);
 
   // 장식품 생성 API
   app.post(
-    "api/trees/:userIdx/decoration",
+    "/api/trees/:userIdx/decoration",
     jwtMiddleware,
     checkUserIdx,
     tree.postDecoration
@@ -16,7 +16,7 @@ module.exports = function (app) {
 
   // 장식품 상세 조회 API
   app.get(
-    "api/trees/:userIdx/decoration/:decorationIdx",
+    "/api/trees/:userIdx/decoration/:decorationIdx",
     jwtMiddleware,
     checkUserIdx,
     tree.getDecoration
@@ -24,7 +24,7 @@ module.exports = function (app) {
 
   // 장식품 삭제 API
   app.delete(
-    "api/trees/decoration/:decorationIdx",
+    "/api/trees/decoration/:decorationIdx",
     jwtMiddleware,
     tree.deleteDecoration
   );
--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -3,18 +3,18 @@ module.exports = function (app) {
   const jwtMiddleware = require("../../../config/jwtMiddleware");
 
   // 이메일 회원가입 API
-  app.post("api/sign-up", user.postSignUp);
+  app.post("/api/sign-up", user.postSignUp);
 
   // 이메일 로그인 API (jwt 생성)
-  app.post("api/sign-in", user.login);
+  app.post("/api/sign-in", user.login);
 
   // 비밀번호 재설정 API
-  app.post("api/reset-password", user.resetPassword);
+  app.post("/api/reset-password", user.resetPassword);
 
   // get: 회원 정보 관리 페이지
   // post: 사용자 개인정보 변경 API
   app
-    .route("/users/edit/:userIdx")
+    .route("/api/users/edit/:userIdx")
     .all(jwtMiddleware)
     .get(user.getUserById)
     .post(user.editUserInfo);
